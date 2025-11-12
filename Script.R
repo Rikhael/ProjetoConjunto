eaf9d2d7150fa5419a09c536266d5179b8f42614
Rikhael <fe_ceacero@id.uff.br>
2025-11-12 10:19:29 -0300

    Adicionando documentação

--- Script.R
+++ Script.R
@@ -1,10 +1,10 @@
 library(readxl)
-library(ggplot2)
-library(usethis)
+library(ggplot2) #pacote para construção de gráficos
 
 dados = read_excel("Base Casos.xlsx")
-ggplot(dados, aes(y = Estatura)) + 
+ggplot(dados, aes(y = Estatura)) +  #construindo boxplot de estatura
   geom_boxplot(fill = "skyblue") +
   labs(title = "Boxplot do da Estatura", y = "Altura (metros)") +
   theme_bw() + 
   theme(axis.text.x = element_blank())
+
